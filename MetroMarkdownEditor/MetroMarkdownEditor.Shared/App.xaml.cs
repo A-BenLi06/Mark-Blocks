2a7f1a270633d437a154af69c200de3a90614b02
benli06 <ben_li06@outlook.com>
2025-11-29 01:45:09 +0800

    v1.0.0 bug:我希望让代码块的高亮遵从vscode的样式, 我希望文件标签右上角能有一个小"x"以关闭该文件, 我希望左侧markdown源码书写页面也有响应式布局. 我不希望在源码里更改一个字符就自动回到开头, 这样根本无法编辑. 我希望appbar里靠左的那组项目与靠右的那组项目交换位置. 我希望界面, 默认字体及字间距, 排版等更加美观, 参考图3. 我希望在appbar里加入导出功能, 导出可选导出到md/html/pdf.

---
+++ MetroMarkdownEditor/MetroMarkdownEditor.Shared/App.xaml.cs
@@ -0,0 +1,130 @@
+using System;
+using MetroMarkdownEditor.ViewModels;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+#if !WINDOWS_PHONE_APP
+using Windows.UI.ApplicationSettings;
+#endif
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+namespace MetroMarkdownEditor
+{
+    public sealed partial class App : Application
+    {
+#if WINDOWS_PHONE_APP
+        private TransitionCollection transitions;
+#endif
+
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        protected override async void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            var rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame { CacheSize = 1 };
+                Window.Current.Content = rootFrame;
+            }
+
+            var locator = Resources["Locator"] as ViewModelLocator;
+            if (locator != null)
+            {
+                await locator.RecentFiles.InitializeAsync();
+                locator.Theme.ApplyThemeToRoot();
+            }
+
+#if !WINDOWS_PHONE_APP
+            SettingsPane.GetForCurrentView().CommandsRequested += OnCommandsRequested;
+#endif
+
+            if (rootFrame.Content == null)
+            {
+#if WINDOWS_PHONE_APP
+                if (rootFrame.ContentTransitions != null)
+                {
+                    transitions = new TransitionCollection();
+                    foreach (var c in rootFrame.ContentTransitions)
+                    {
+                        transitions.Add(c);
+                    }
+                }
+
+                rootFrame.ContentTransitions = null;
+                rootFrame.Navigated += RootFrame_FirstNavigated;
+#endif
+
+                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+
+            Window.Current.Activate();
+        }
+
+#if WINDOWS_PHONE_APP
+        private void RootFrame_FirstNavigated(object sender, NavigationEventArgs e)
+        {
+            var rootFrame = sender as Frame;
+            rootFrame.ContentTransitions = transitions ?? new TransitionCollection { new NavigationThemeTransition() };
+            rootFrame.Navigated -= RootFrame_FirstNavigated;
+        }
+#endif
+
+#if !WINDOWS_PHONE_APP
+        private void OnCommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            args.Request.ApplicationCommands.Add(new SettingsCommand("theme", "Theme", _ => ShowThemeSettings()));
+        }
+
+        private void ShowThemeSettings()
+        {
+            var flyout = new ThemeSettings();
+            var locator = Resources["Locator"] as ViewModelLocator;
+            if (locator != null)
+            {
+                flyout.DataContext = locator.Theme;
+            }
+
+            flyout.Show();
+        }
+#endif
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            deferral.Complete();
+        }
+
+#if WINDOWS_PHONE_APP
+        protected override async void OnActivated(IActivatedEventArgs args)
+        {
+            var locator = Resources["Locator"] as ViewModelLocator;
+
+            var openArgs = args as FileOpenPickerContinuationEventArgs;
+            if (openArgs != null && locator != null)
+            {
+                await locator.Editor.HandleOpenPickerContinuation(openArgs);
+            }
+            else
+            {
+                var saveArgs = args as FileSavePickerContinuationEventArgs;
+                if (saveArgs != null && locator != null)
+                {
+                    await locator.Editor.HandleSavePickerContinuation(saveArgs);
+                }
+            }
+
+            base.OnActivated(args);
+        }
+#endif
+    }
+}
---
+++ MetroMarkdownEditor/MetroMarkdownEditor.Windows/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+using MetroMarkdownEditor.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace MetroMarkdownEditor
+{
+    public sealed partial class MainPage : Page
+    {
+        private MainViewModel ViewModel
+        {
+            get { return DataContext as MainViewModel; }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+            Unloaded += OnUnloaded;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (ViewModel != null)
+            {
+                ViewModel.NavigationRequested += OnNavigationRequested;
+                await ViewModel.InitializeAsync();
+            }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel != null)
+            {
+                ViewModel.NavigationRequested -= OnNavigationRequested;
+                ViewModel.NavigationRequested += OnNavigationRequested;
+            }
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel != null)
+            {
+                ViewModel.NavigationRequested -= OnNavigationRequested;
+            }
+        }
+
+        private void OnNavigationRequested(object sender, EditorNavigationRequest e)
+        {
+            Frame.Navigate(typeof(EditorPage), e);
+        }
+    }
+}
